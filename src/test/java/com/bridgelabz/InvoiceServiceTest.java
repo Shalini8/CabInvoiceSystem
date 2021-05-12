3239e76f642ac7cb13b5d21349294e619fbe0b66
Shalini8 <shalu8mar@gmail.com>
2021-05-12 19:30:26 +0530

    TC2_checkForMinimumFare_calulateTotalFare

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -13,5 +13,14 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25,fare,0.0);
     }
+    @Test
+    void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5,fare,0.0);
+
+    }
 
 }
